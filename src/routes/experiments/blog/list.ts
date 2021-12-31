d278a59e062a90fcab45a61a7247f0e47b611160
Henrik Sjööh <henriiik@gmail.com>
2021-12-31 09:23:32 +0100

    appease the build command

--- src/routes/experiments/blog/list.ts
+++ src/routes/experiments/blog/list.ts
@@ -1,16 +1,17 @@
 import type { RequestHandler } from "@sveltejs/kit";
-import { opendir } from "fs";
+import { readdir } from "fs";
 import { promisify } from "util";
-const opendir2 = promisify(opendir);
+const readdir2 = promisify(readdir);
 
 export type BlogPost = { path: string; name: string; date: string };
 
 export const get: RequestHandler = async ({ params }) => {
   let posts: BlogPost[] = [];
 
-  const dir = await opendir2("./src/routes/experiments/blog/posts");
-  for await (const entry of dir) {
-    const path = entry.name.replace(/.svelte$/, "");
+  const dir = await readdir2("./src/routes/experiments/blog/posts");
+
+  for (const entry of dir) {
+    const path = entry.replace(/.svelte$/, "");
     let split = path.replace(/_/g, " ").split("|");
     posts.push({ path, name: split[1], date: split[0] });
   }
